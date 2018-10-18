b8dae9b4b31272f2c3eeaf6382085e0a9a5414bd
pavel_cpp <pasha.dunin.94@gmail.com>
2018-10-19 04:44:00 +0600

    changes

--- index.js
+++ index.js
@@ -4,18 +4,16 @@ var http = require('http'),
 
 http.createServer(function(request, response) {
     if(request.url == '/data'){
-        var json = fileSystem.readFileSync('coords.json')
-        var roots = fileSystem.readFileSync('plants_dragon_root.json')
-
+        var chests = fileSystem.readFileSync('data/chests.json')
+        var roots = fileSystem.readFileSync('data/roots.json')
+        var kingSorrels = fileSystem.readFileSync('data/ks.json')
+        var goblins = fileSystem.readFileSync('data/goblins.json')
         response.writeHead(200, {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'})
-        var jsonResponse = {chests: JSON.parse(json), roots: JSON.parse(roots)}
-
-
+        var jsonResponse = {chests: JSON.parse(chests), roots: JSON.parse(roots), kingSorrels: JSON.parse(kingSorrels), goblins: JSON.parse(goblins)}
         response.end(Buffer.from(JSON.stringify(jsonResponse)))
     } else {
-        console.log('url', request.url)
-        if(request.url != '/'){
-            var filePath = path.join(__dirname, request.url);
+        if(request.url != '/' && request.url.match('/[0-9]/[0-9]?[0-9]_[0-9]?[0-9].jpg')){
+            var filePath = path.join(__dirname, 'tiles/' + request.url);
             try{
                 var stat = fileSystem.statSync(filePath);
                 var name = request.url.split('.')[0]
@@ -31,6 +29,7 @@ http.createServer(function(request, response) {
             }
         } else {
             response.writeHead(200)
+            response.write('Doesnt match any!')
             response.end()
         }
     }
